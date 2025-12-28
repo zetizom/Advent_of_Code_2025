6c6b473c5fc99db3b0bde5d91483cafddb26c6d8
zetizom <90217209+zetizom@users.noreply.github.com>
2025-12-28 23:14:19 +0900

    3일차 part 1 및 디버깅 툴 추가완료

---
+++ Day 3/AoC_3.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+    FILE *fp = fopen("AoC_3.txt", "r"); 
+    
+    if(fp == NULL) {
+        printf("파일 읽기 실패\n");
+        return 1;
+    }
+    char str[120];
+    int result = 0;
+
+    while(fgets(str, sizeof(str), fp) != NULL) {
+        char start = str[0];
+        char end = str[1];
+        int max = 0;
+        if (start == '\n') {
+            continue;
+        }
+
+        for(int i = 0; str[i] != '\0'; i++) {
+            for (int j = i+1; str[j] != '\0'; j++) {
+                start = str[i];
+                end = str[j];
+                int num = 10* (start - 48) + (end - 48);
+                if (num > max) {
+                    max = num;
+                }
+            }
+        } 
+
+        result += max;
+        printf("%d, %d\n", max, result);
+
+    }
+
+    printf("%d\n", result);
+}
